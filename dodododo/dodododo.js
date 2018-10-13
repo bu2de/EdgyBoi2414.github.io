53bbfe0e001a16f3711bd55fc3dd03a0fb8b434e
EdgyBoi2414 <44114101+EdgyBoi2414@users.noreply.github.com>
2018-10-13 17:04:35 +0100

    Yeet

--- dodododo/dodododo.js
+++ dodododo/dodododo.js
@@ -1,5 +1,6 @@
 var sans;
 var megalovainia;
+var isPlaying = false;
 
 function preload() {
   soundFormats('mp3', 'ogg');
@@ -9,15 +10,23 @@ function preload() {
 function setup() {
     noCanvas();
     sans = createImg('https://slm-assets2.secondlife.com/assets/12703379/view_large/sans_512.jpg?1446743951');
-    sans.mouseClicked(playSound);
+    
 }
 
 function draw() {
-    
+    if (sans.mouseClicked(true)) {
+      if (isPlaying == true) {
+        stopSound();
+        return;
+      } else {
+        playSound();
+      }
+    }
 }
 
 function playSound() {
     megalovainia.play();
+    isPlaying = true;
 }
 
 function stopSound() {
